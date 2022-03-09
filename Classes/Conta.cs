1842d589f84e710be0b1c18c7d5b45cd840c37bc
Leandro-SI <llmanutencao@bol.com.br>
2022-03-08 21:24:31 -0300

    add classe

---
+++ Classes/Conta.cs
@@ -0,0 +1,53 @@
+using System;
+using Leandro.Bank.Enum;
+
+namespace Leandro.Bank.Classes
+{
+    public class Conta
+    {
+        private TipoConta TipoConta { get; set; }
+        private double Saldo { get; set; }
+        private double Credito { get; set; }
+        private string Nome { get; set; }
+
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome) 
+        {
+            this.TipoConta = tipoConta;
+            this.Saldo = saldo;
+            this.Credito = credito;
+            this.Nome = nome;
+        }
+
+        public bool Sacar(double valorSaque)
+        {
+            if (valorSaque > (this.Saldo + this.Credito))
+            {
+                Console.WriteLine("Saldo insuficiente");
+                return false;
+            }
+
+            this.Saldo -= valorSaque;
+
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+
+            return true;
+        }
+
+        public void Depositar(double valorDeposito)
+        {
+            this.Saldo += valorDeposito;
+
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+
+        }
+
+        public void Transferencia(double valorTransferencia, Conta contaDestino)
+        {
+            if (this.Sacar(valorTransferencia))
+            {
+                contaDestino.Depositar(valorTransferencia);
+            }
+        }
+
+    }
+}
---
+++ Enum/TipoConta.cs
@@ -0,0 +1,8 @@
+namespace Leandro.Bank.Enum
+{
+    public enum TipoConta
+    {
+        PessoaFisica = 1,
+        PessoaJuridica = 2,
+    }
+}
